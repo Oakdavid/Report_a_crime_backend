f4a5a54aea59bbd12a72b828067cb6f927f5b9e2
David Oduntan <davidoduntan1@gmail.com>
2024-11-06 17:33:26 +0000

    initialCommit

--- obj/Debug/net8.0/Report_A_Crime.AssemblyInfo.cs
+++ obj/Debug/net8.0/Report_A_Crime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9457d9af070f826ab59f963145006bbfb68108e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02be8ed061cd6032f431208021e4ec3ab0db15b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
