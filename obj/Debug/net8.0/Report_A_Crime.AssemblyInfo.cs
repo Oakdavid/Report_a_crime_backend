92cb333eb2db26e5ba21e4ff3efdd8435665f67c
Oduntan David <davidoduntan1@gmail.com>
2024-12-04 15:26:49 +0100

    working on geolocation

--- obj/Debug/net8.0/Report_A_Crime.AssemblyInfo.cs
+++ obj/Debug/net8.0/Report_A_Crime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e7be7943857bf7210c97f2219ab4380738aa1db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937bcf484bc88d39a47a005a1314f6bc30358412")]
 [assembly: System.Reflection.AssemblyProductAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
