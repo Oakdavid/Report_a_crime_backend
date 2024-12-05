da6e28dbc5eab0b387070543426706f63771c049
Oduntan David <davidoduntan1@gmail.com>
2024-12-05 19:16:26 +0100

    geolocationDto

---
+++ Models/Dtos/GeolocationDto.cs
@@ -0,0 +1,18 @@
+﻿using Report_A_Crime.Models.Entities;
+
+namespace Report_A_Crime.Models.Dtos
+{
+    public class GeolocationDto : BaseResponse
+    {
+        public Guid GeolocationId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public Guid ReportId { get; set; }
+    }
+
+    public class GeolocationRequestModel
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
--- obj/Debug/net8.0/Report_A_Crime.AssemblyInfo.cs
+++ obj/Debug/net8.0/Report_A_Crime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f498f29f8ce60fe6a1bd687203ddb20af1ca3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb79c82e2de82cbc07c079ee528f56982b5feeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
