b8e5ad988e97fb3bbc90f822f9ffe8d7577ed347
Oduntan David <davidoduntan1@gmail.com>
2025-01-01 03:10:35 +0100

    geolocation controller

---
+++ Controllers/GeolocationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Report_A_Crime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeolocationController : ControllerBase
+    {
+    }
+}
