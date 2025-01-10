7b4116932cba9803340df2c96e98f8ea33811c1a
Oduntan David <davidoduntan1@gmail.com>
2025-01-10 20:46:13 +0100

    geolocation is yet to work

---
+++ Migrations/20250110192706_Geolocation modification.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Report_A_Crime.Migrations
+{
+    /// <inheritdoc />
+    public partial class Geolocationmodification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "Geolocations",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "Geolocations");
+        }
+    }
+}
