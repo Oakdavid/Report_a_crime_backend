20dda5ebe61076a6b0da2097e443030dfdad8974
Oduntan David <davidoduntan1@gmail.com>
2025-02-04 16:31:46 +0100

    modifi

--- obj/Debug/net8.0/Report_A_Crime.AssemblyInfo.cs
+++ obj/Debug/net8.0/Report_A_Crime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c25048bc4c0249e3ce17a43fa67abf89dcc3815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dbe2b5e8ca8ddd0f981c70387d3acb082deca2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Report_A_Crime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
